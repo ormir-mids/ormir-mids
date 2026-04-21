c76f8fa7e4c67e0b74a6649674fefe8b43613f68
Francesco Santini <francesco.santini@gmail.com>
2026-04-21 15:21:47 +0200

    Restore previous behavior when all converters are imported in the converter submodule. Version bump.

--- src/ormir_mids/converters/__init__.py
+++ src/ormir_mids/converters/__init__.py
@@ -1,4 +1,4 @@
-from ..converter_base.abstract_converter import RootConverter
+from ..converter_base.abstract_converter import Converter, RootConverter
 
 # just importing all the converters builds the converter tree.
 # If you create a new converter, make sure to import it here
@@ -18,14 +18,25 @@ from . import mese_philips
 from . import mese_siemens
 from . import quantitative_maps
 
-from .quantitative_maps import T1Converter, T2Converter, FFConverter, B0Converter, B1Converter
+# automatically import all Converters in this namespace
+def _collect_subclasses(cls):
+    result = []
+    for sub in cls.__subclasses__():
+        result.append(sub)
+        result.extend(_collect_subclasses(sub))
+    return result
+
+for _cls in _collect_subclasses(Converter):
+    globals()[_cls.__name__] = _cls
+
+del _cls, _collect_subclasses
 
 # print converter tree
-def print_lineage(converter, level=0):
+def print_lineage(converter=RootConverter, level=0):
     print('  ' * level + converter.get_name())
     for child in converter.get_children():
         print_lineage(child, level + 1)
 
 # Uncomment the line below to print the converter tree
 # This can be useful for debugging or understanding the converter hierarchy.
-# print_lineage(RootConverter)
+# print_lineage()
